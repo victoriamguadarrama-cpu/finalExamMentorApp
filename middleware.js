6af42d88f3d1b2bd7851e2e11759a510514f0278
Victoria Guadarrama <a02250411@aggies.usu.edu>
2025-12-02 15:01:51 -0700

    Fix log directory creation for deployment

--- middleware.js
+++ middleware.js
@@ -4,6 +4,12 @@ const path = require("path");
 
 // Set files/paths
 const logDirectory = path.join(__dirname, "log");
+
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory, { recursive: true });
+}
+
+
 const logFile = path.join(logDirectory, "logRequests.txt");
 
 // Create a write stream (in append mode)
